757109e0ceabced7b48bca6b2efddac96c94d96e
alphonseb <alphonse.bouy@gmail.com>
2020-11-17 01:12:04 +0100

    :sparkles: Input Atom

---
+++ src/components/01_atoms/TextInput/TextInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <woof-text
+    class="a-text-input"
+    :class="{ 'a-text-input--error': hasError }"
+    tag="input"
+    type="input"
+    :input-type="inputType"
+  />
+</template>
+
+<script>
+import WoofText from "@/components/01_atoms/WoofText/WoofText.vue";
+export default {
+  name: "TextInput",
+  components: {
+    WoofText
+  },
+  props: {
+    hasError: {
+      type: Boolean,
+      default: false
+    },
+    inputType: {
+      type: String,
+      default: "text"
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./TextInput.scss";
+</style>
