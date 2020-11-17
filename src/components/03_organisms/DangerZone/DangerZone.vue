f1595decdcb795b087264feee2e882aeb33dd25f
alphonseb <alphonse.bouy@gmail.com>
2020-11-17 15:19:30 +0100

    :sparkles: danger zone organism

---
+++ src/components/03_organisms/DangerZone/DangerZone.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="o-danger-zone">
+    <div class="o-danger-zone__text-wrapper">
+      <woof-text class="o-danger-zone__title" tag="h3" type="small-l"
+        >Delete my account</woof-text
+      >
+      <woof-text class="o-danger-zone__body" tag="p" type="body-m">
+        Are you sure you want to delete your account ? <br />
+        You’ll permanently loose your informations.
+      </woof-text>
+    </div>
+    <woof-button type="danger" buttonTitle="delete" icon="trash" />
+  </div>
+</template>
+
+<script>
+import WoofText from "@/components/01_atoms/WoofText/WoofText.vue";
+import WoofButton from "@/components/02_molecules/WoofButton/WoofButton.vue";
+export default {
+  name: "DangerZone",
+  components: {
+    WoofText,
+    WoofButton
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./DangerZone.scss";
+</style>
