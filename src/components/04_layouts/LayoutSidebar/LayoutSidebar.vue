9146dc0b880f35439b29ec4104b4ed9ed1055c5e
alphonseb <alphonse.bouy@gmail.com>
2020-11-17 15:20:09 +0100

    :sparkles: layout with sidebar

---
+++ src/components/04_layouts/LayoutSidebar/LayoutSidebar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="l-sidebar">
+    <side-bar />
+    <main style="position: relative;">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script>
+import SideBar from "@/components/03_organisms/SideBar/SideBar.vue";
+export default {
+  name: "LayoutSidebar",
+  components: {
+    SideBar
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./LayoutSidebar.scss";
+</style>
