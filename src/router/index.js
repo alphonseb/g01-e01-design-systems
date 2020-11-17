197bf377dd16cf39dc60314efc9195cdd14cc3cf
alphonseb <alphonse.bouy@gmail.com>
2020-11-17 15:21:34 +0100

    :wrench: update router config

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Account from "../views/Account.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about/",
-    name: "about",
-    component: Home
+    name: "Account",
+    component: Account
   }
 ];
 
