aacfebd75c7da84ae20e8e724f92fa6a11707818
alphonseb <alphonse.bouy@gmail.com>
2020-11-17 01:11:30 +0100

    :sparkles: Text Atom

---
+++ src/components/01_atoms/WoofText/WoofText.vue
@@ -0,0 +1,29 @@
+<template>
+  <component :is="tag" :class="`a-text--${type}`" :type="inputType">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  name: "WoofText",
+  props: {
+    tag: {
+      type: String,
+      default: "p"
+    },
+    type: {
+      type: String,
+      default: "body-m"
+    },
+    inputType: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./WoofText.scss";
+</style>
