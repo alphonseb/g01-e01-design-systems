d094d508c5e0fd22532f1d8d625be14ba8310d1f
alphonseb <alphonse.bouy@gmail.com>
2020-11-17 01:13:25 +0100

    :sparkles: Form Field molecule

---
+++ src/components/02_molecules/FormField/FormField.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="m-form-field">
+    <woof-text tag="label" type="small-l">{{ label }}</woof-text>
+    <text-input :input-type="inputType" />
+  </div>
+</template>
+
+<script>
+import WoofText from "@/components/01_atoms/WoofText/WoofText.vue";
+import TextInput from "@/components/01_atoms/TextInput/TextInput.vue";
+export default {
+  name: "FormField",
+  components: {
+    WoofText,
+    TextInput
+  },
+  props: {
+    label: {
+      type: String,
+      default: "label"
+    },
+    inputType: {
+      type: String,
+      default: "text"
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./FormField.scss";
+</style>
