412692bdc2ccdb63e9124a42a870f246040951bb
alphonseb <alphonse.bouy@gmail.com>
2020-11-17 10:22:12 +0100

    :wrench: input config

---
+++ src/components/02_molecules/TextInput/TextInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="['m-text-input', { 'm-text-input--error': hasError }]">
+    <input :type="type" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextInput",
+  props: {
+    hasError: {
+      type: Boolean,
+      default: false
+    },
+    type: {
+      type: String,
+      default: "text",
+      validator(value) {
+        const validTextStyles = ["text", "email"];
+        return validTextStyles.includes(value);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./TextInput.scss";
+</style>
