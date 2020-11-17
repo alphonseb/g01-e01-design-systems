e057d8a8a7fe3d70935f822daa4d678a1eb020ab
alphonseb <alphonse.bouy@gmail.com>
2020-11-17 01:13:43 +0100

    :sparkles: Button  molecule

---
+++ src/components/02_molecules/WoofButton/WoofButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="m-button">
+    <woof-text tag="span" type="body-l">
+      {{ buttonTitle }}
+    </woof-text>
+  </button>
+</template>
+
+<script>
+import WoofText from "@/components/01_atoms/WoofText/WoofText.vue";
+export default {
+  name: "WoofButton",
+  components: {
+    WoofText
+  },
+  props: {
+    buttonTitle: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./WoofButton.scss";
+</style>
