84919190a35c2951a810c6479228b1ceae472f93
alphonseb <alphonse.bouy@gmail.com>
2020-11-17 01:14:21 +0100

    :sparkles: Form organism

---
+++ src/components/03_organisms/LoginForm/LoginForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="o-login-form">
+    <div class="o-login-form__field-container">
+      <form-field label="e-mail" input-type="email" />
+      <form-field label="full name" />
+    </div>
+    <woof-button button-title="save" />
+  </div>
+</template>
+
+<script>
+import FormField from "@/components/02_molecules/FormField/FormField.vue";
+import WoofButton from "@/components/02_molecules/WoofButton/WoofButton.vue";
+export default {
+  name: "LoginForm",
+  components: {
+    FormField,
+    WoofButton
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./LoginForm.scss";
+</style>
